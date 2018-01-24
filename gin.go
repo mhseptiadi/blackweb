655e061f972a14fd2d332982b92130c6ba9a3af8
Muhammad Hasan Septiadi <septiadi@as-MacBook-Pro-2.local>
2018-01-24 22:58:02 +0700

    add routes and compile

--- gin.go
+++ gin.go
@@ -1,6 +1,7 @@
 package main
 
 import "github.com/gin-gonic/gin"
+import "./routes"
 
 func main() {
 	r := gin.Default()
@@ -9,5 +10,6 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/test", test.Run)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ routes/test.go
@@ -0,0 +1,10 @@
+package test
+
+import "github.com/gin-gonic/gin"
+// import "fmt"
+
+func Run(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
